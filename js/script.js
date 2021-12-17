442c4ed295c42132b5ae077c21e79a62618f8c8d
Ye Ni Zhiao <afifrider507@gmail.com>
2021-12-17 20:41:45 +0700

    new commit

---
+++ js/script.js
@@ -0,0 +1,100 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startGame() {
+  console.log("ok");
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    let button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    answerButtonsElement.appendChild(button);
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+}
+
+const questions = [
+  {
+    question: "Kapan Indonesia merdeka?",
+    answers: [
+      { text: "17 Agustus 1945", correct: true },
+      { text: "16 Agustus 1945", correct: false },
+    ],
+  },
+  {
+    question: "Apa warna bendera Indonesia?",
+    answers: [
+      { text: "Merah Putih", correct: true },
+      { text: "Putih Merah", correct: false },
+    ],
+  },
+  {
+    question : "Apa bahasa nasional Indonesia?",
+    answers : [
+      { text : "Bahasa Jerman", correct: false},
+      { text : "Bahasa Indonesia", correct : true},
+    ],
+  },
+];
